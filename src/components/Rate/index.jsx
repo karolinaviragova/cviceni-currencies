43953f19207f47b83eb8258108f59eb0e07e170b
Karolina Viragova <karolinavi94@gmail.com>
2025-05-11 15:42:48 +0200

    solution

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,4 +1,6 @@
 import './style.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const currencies = {
   USD: { CZK: 23.823 },
@@ -7,11 +9,30 @@ const currencies = {
 };
 
 export const Rate = ({from}) => {
+   const [rate, setRate] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch (`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+      const responseData = await response.json();
+      setRate(responseData.rates.CZK)
+    };
+    fetchData();
+  }, [from])
+  
+
   return (
     <div className="rate">
       <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">{currencies[from].CZK} CZK</div>
+      <div className="rate__value">
+      {rate !== null ? `${rate} CZK` : "Načítám..."}
+</div>
+
     </div>
   );
 };
+
+/*
+{currencies[from].CZK} 
+*/
